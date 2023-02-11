81b296dd43edbb570c68b1d8076668c631362785
Namfoodle <pedro_o99@hotmail.com>
2023-02-11 17:22:15 -0300

    Images now use png

--- src/builder/ImageBuilder.js
+++ src/builder/ImageBuilder.js
@@ -1,15 +1,15 @@
 'use-strict'
 
-import potion1 from "../assets/images/potions/potions (1).jpg";
-import potion2 from "../assets/images/potions/potions (2).jpg";
-import potion3 from "../assets/images/potions/potions (3).jpg";
-import potion4 from "../assets/images/potions/potions (4).jpg";
-import potion5 from "../assets/images/potions/potions (5).jpg";
-import potion6 from "../assets/images/potions/potions (6).jpg";
-import potion7 from "../assets/images/potions/potions (7).jpg";
-import potion8 from "../assets/images/potions/potions (8).jpg";
-import potion9 from "../assets/images/potions/potions (9).jpg";
-import potion10 from "../assets/images/potions/potions (10).jpg";
+import potion1 from "../assets/images/potions/potions (1).png";
+import potion2 from "../assets/images/potions/potions (2).png";
+import potion3 from "../assets/images/potions/potions (3).png";
+import potion4 from "../assets/images/potions/potions (4).png";
+import potion5 from "../assets/images/potions/potions (5).png";
+import potion6 from "../assets/images/potions/potions (6).png";
+import potion7 from "../assets/images/potions/potions (7).png";
+import potion8 from "../assets/images/potions/potions (8).png";
+import potion9 from "../assets/images/potions/potions (9).png";
+import potion10 from "../assets/images/potions/potions (10).png";
 
 const potionImages = [
     potion1,
